000f9812fd3948f9ada5d28a67b86cda11e2f75e
kun.ma <makun5201@163.com>
2022-08-14 13:18:15 +0800

    test

---
+++ firstwork/test.py
@@ -0,0 +1,103 @@
+'''
+我炸了
+'''
+
+# days = ['Monday', 'Tuesday', 'Wednesday',
+#         'Thursday', 'Friday']
+# if True:
+#     print(days)
+# else:
+#     print("False")
+#
+# x = '2'
+# y = '444'
+# print(x)
+# print(y)
+#
+# print(x,end='')
+# print(y,end='')
+
+
+num = 100
+weignt = 100.0
+name = 'makin'
+
+print(num)
+print(weignt)
+print(name)
+
+a,b,c = 1,2.3,'我找了'
+
+print(c)
+
+num1 = 1
+num2 = 12
+print(num1)
+
+del num1
+
+# print(num1)
+
+s = 'asfjdsaf;h;sdafsd'
+print(s[0:])
+print(s[0:3] * 2)
+print(s[0::2])
+
+
+t = ['q','a','w','33','sss']
+t[1] = 6666
+print(t[0:])
+print(t[1:3])
+print(t[0::2])
+
+
+r = ('3343','sdfsdfsd',424,'sfffffffff',2.3)
+# r[1] = 6666
+print(r[0:])
+print(r[0::2])
+
+print('----------------------')
+d = {'a':'1111',b:222}
+print(d)
+print(d[b])
+print(d.keys())
+print(d.values())
+
+print('-------------------------')
+print(list('22'))
+print(tuple['sdfadsf'])
+print(hex(55555))
+print(oct(22))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
